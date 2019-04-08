426ed697e76d88411d9991c7f691701bbcc832ca
AfekSurdo <48516118+AfekSurdo@users.noreply.github.com>
2019-04-08 23:53:42 +0300

    Add AngelinaJulie page
    Add Angelina Julie page

--- app/src/main/java/com/example/contacts/page3.java
+++ app/src/main/java/com/example/contacts/page3.java
@@ -16,15 +16,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class page3 extends AppCompatActivity {
 
+    int         like_cnt = 0;
+    ImageButton like_button;
+    TextView    like_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
+
+
+        like_button = (ImageButton) findViewById(R.id.likebutton);
+        like_view   = (TextView)    findViewById(R.id.like_text_view);
+
+        like_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                like_cnt++;
+                like_view.setText(like_cnt + " ");
+
+            }
+        });
 
     }
 }
