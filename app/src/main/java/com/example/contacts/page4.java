2833ee1d2af726e5ebdaaf87e87a1040f57b4d9d
unknown <adirz101@gmail.com>
2019-04-09 12:18:04 +0300

    add new page4

--- app/src/main/java/com/example/contacts/page4.java
+++ app/src/main/java/com/example/contacts/page4.java
@@ -16,13 +16,34 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 public class page4 extends AppCompatActivity {
-
+    int _click=650;
+    TextView like,unlike;
+    Button B;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page4);
+        B = (Button) findViewById(R.id.Like);
+        like = (TextView) findViewById(R.id.textView3);
+        unlike = (Button) findViewById(R.id.unLike);
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                like.setText(++_click+"");
+            }
+        });
+        unlike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(_click>0)
+                    like.setText(--_click+"");
+            }
+        });
     }
 }
+
+
